2b21dea1dc42798075cb3a6496a96d2da00134ad
lowefredriksson <lowe@art-is.se>
2018-05-23 10:53:10 +0200

    touch recognition on canvas

--- real-time-video/app.js
+++ real-time-video/app.js
@@ -3,6 +3,8 @@ var canvas = document.getElementById('canvas-video');
 var context = canvas.getContext('2d');
 var map = document.getElementById('canvas-map');
 var mapContext = map.getContext('2d');
+var currentDpDisplay = document.getElementById('current-dp');
+var destinationDpDisplay = document.getElementById('destination-dp');
 
 var dropoffPoints = [
   { 
@@ -36,7 +38,10 @@ var dropoffPoints = [
     name: "Point 5"
   }
 ]
-const dropoffPointSize = { width: 10, height: 0}
+const dropoffPointSize = { width: 10, height: 0};
+const dropoffTouchRecognitionSize = { width: 40, height: 40 };
+let currentDropoffpoint = null;
+
 
 function drawDropoffPoints(dropoffPoints) {
 
@@ -125,3 +130,53 @@ ws.onmessage = function (json) {
     }
 }
 
+function isInside(pos, rect){
+  return pos.x > rect.x-(rect.width/2) && pos.x < rect.x+(rect.width/2) && pos.y < rect.y+(rect.height/2) && pos.y > rect.y-(rect.height/2)
+}
+
+function isInsideDropoffPoint(pos) {
+  for (var i = 0; i < dropoffPoints.length; i++) {
+    const dp = dropoffPoints[i];
+    const rect = { 
+      x: dp.x, 
+      y: dp.y, 
+      width: dropoffTouchRecognitionSize.width, 
+      height: dropoffTouchRecognitionSize.height 
+    };
+    console.log("rect", rect);
+    console.log("mousepos", pos);
+    if (isInside(pos, rect)) {
+      return dp;
+    }
+  }
+  return null;
+}
+
+function getMousePos(canvas, event) {
+  var rect = canvas.getBoundingClientRect();
+  console.log("rect", rect);
+  return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+  };
+}
+
+function updateDisplay() {
+  destinationDpDisplay.innerHTML = destinationDropoffpoint.name
+}
+
+map.addEventListener('click', function(evt) {
+  var mousePos = getMousePos(map, evt);
+  var selectedDp = isInsideDropoffPoint(mousePos);
+  if (selectedDp) {
+    console.log("hej", selectedDp)
+    destinationDropoffpoint = selectedDp
+    updateDisplay();
+  }
+
+  /*if (isInside(mousePos,rect)) {
+      alert('clicked inside rect');
+  }else{
+      alert('clicked outside rect');
+  }*/ 
+}, false);
