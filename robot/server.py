bf9c5b625982845b6e607312b6f7c243a1069de7
Dinh Dung <dinhanhdung1996@gmail.com>
2018-05-25 16:55:36 +0700

    adding server from robot

---
+++ robot/server.py
@@ -0,0 +1,92 @@
+from bluetooth import *
+import json
+import time
+import threading
+
+
+mac_address = "D4:36:39:D1:E3.1F"
+port = 1
+max_data = 2048
+
+
+def connect_to_ev3(mac_address, port):
+    server_socket = BluetoothSocket(RFCOMM)
+    server_socket.connect((mac_address, port))
+    #server_socket.recv(1024)
+    return server_socket
+
+
+class EvConnect:
+    connector = None
+
+    @staticmethod
+    def get_instance():
+        if EvConnect.connector is None:
+            EvConnect.connector = EvConnect()
+        return EvConnect.connector
+
+    @staticmethod
+    def clear_instance():
+        EvConnect.connector = None
+
+    def __init__(self):
+        self.data = ""
+        self.mac_address = mac_address
+        self.port = port
+        self.server_socket = None
+        self.set_up_connection()
+        self.receive_message = ""
+        self.send_message = ""
+
+    def set_up_connection(self):
+        self.server_socket = connect_to_ev3(mac_address, port)
+
+    def get_data(self):
+        if self.server_socket is None:
+            return
+        string_data = str(self.server_socket.recv(max_data))
+        print(string_data)
+        return string_data
+        # do something to back to client
+
+    def send_data(self, str_data):
+        if self.server_socket is None:
+            return None
+        self.server_socket.send(str_data)
+        self.send_message = str_data
+
+
+class EvListenThread(threading.Thread):
+    def run(self):
+        ev3_connector = EvConnect.get_instance()
+        while ev3_connector.server_socket is None:
+            continue
+        while True:
+            str_message = ev3_connector.get_data()
+            #do smth back to client
+            print(str_message)
+
+
+def take_fake_input():
+    print("Input the list of codes. If you want to stop push -1")
+    list_action = []
+    while True:
+        a = input(int)
+        if a == -1:
+            break
+    mess = {}
+    mess['list_action'] = list_action
+    mess['default'] = 'on'
+    return mess
+
+
+ev3_connect = EvConnect.get_instance()
+ev3_connect.set_up_connection()
+ev3_listener = EvListenThread(name="listener")
+ev3_listener.start()
+
+fake_mess = take_fake_input()
+ev3_connect.send_data(json.dumps(fake_mess))
+
+
+
